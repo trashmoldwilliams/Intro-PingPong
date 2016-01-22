24c8781b8a024464ffea2f95ed0d1d174a1e107e
Will Johnson <student@epicodus.com>
2016-01-22 07:58:10 -0800

    Add spec to check if numbers divisible by both 3 and 5 output as 'pingpong'

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 var pingPong = function(inputNumber) {
-  if (inputNumber % 3 === 0) {
+  if (inputNumber % 3 === 0 && inputNumber % 5 === 0) {
+    return "pingpong"
+  } else if (inputNumber % 3 === 0) {
     return "ping";
   } else if (inputNumber % 5 === 0) {
     return "pong";
--- spec/specs.js
+++ spec/specs.js
@@ -5,4 +5,7 @@ describe('pingPong', function() {
   it("outputs 'pong' for numbers divisible by 5", function () {
     expect(pingPong(10)).to.equal("pong")
   });
+  it("outputs 'pingpong' for numbers divisible by both 3 and 5", function () {
+    expect(pingPong(15)).to.equal("pingpong")
+  });
 });

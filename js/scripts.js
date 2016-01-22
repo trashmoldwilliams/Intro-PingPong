7dde3dc8b08860128823523ef44df4b9f8108090
Will Johnson <student@epicodus.com>
2016-01-22 08:42:31 -0800

    Refactor code with list point addition removal from pingPong and addition to for loop.

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,19 @@ $(document).ready(function() {
   $("form#pingPong").submit(function(event) {
     var totalInputNumber = parseInt($("input#totalInputNumber").val());
     for (var i = 1; i <= totalInputNumber; i++) {
-      $("ul#pingPongList").append(pingPong(totalInputNumber));
+      $("ul#pingPongList").append("<li>" + pingPong(totalInputNumber + "</li>"));
     }
   });
 });
 
 var pingPong = function(currentInputNumber) {
   if (currentInputNumber % 3 === 0 && currentInputNumber % 5 === 0) {
-    return "<li>'pingpong'</li>"
+    return 'pingpong';
   } else if (currentInputNumber % 3 === 0) {
-    return "<li>'ping'</li>";
+    return 'ping';
   } else if (currentInputNumber % 5 === 0) {
-    return "<li>'pong'</li>";
+    return 'pong';
   } else {
-    return "<li>" + currentInputNumber + "</li>";
+    return currentInputNumber;
   }
 }
